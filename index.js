58765f37b29c9bcd308176a59ad5e5e5ef0e4ebd
Nivetha-deve <nivethashree028@gmail.com>
2024-07-11 21:55:22 +0530

    changes

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ app.use(cors());  /* To avoid cross-origin error */
 app.use(express.json());
 
 const PORT = process.env.PORT || 4100;
-const URL = process.env.DB;
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbCluster = process.env.DB_CLUSTER;
+const dbName = process.env.DB_NAME;
+
+// const URL = process.env.DB;
 console.log('DB Connection URL:', process.env.DB);
 
-if (!URL) {
+if (!dbUser || !dbPassword || !dbCluster || !dbName) {
   console.error('MongoDB connection URL is not defined');
- 
-  process.exit(1); // Exit the application if the URL is not defined
+  process.exit(1); // Exit the process with failure
 }
+
+const URL = `mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
 
 const mongoose = require('mongoose');
 mongoose.connect(URL, {
